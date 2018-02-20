c9ab6930ea9c0d021cec0d92041cb0a1359c0e61
Mcatzer <mcatze@mail.ru>
2018-02-20 20:43:33 +0300

    Initial commit - RLE
    Run Length Encoding in C#. With tests.

---
+++ RunLengthEncoding/RunLengthEncoding/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunLengthEncoding
+{
+    public class RunLengthEncoding
+    {
+        public string Encode(string inpLine)
+        {
+            try
+            {
+                int count = 1;
+                char prevchar = char.MinValue;
+                List<string> lst = new List<string>();
+                foreach (char character in inpLine)
+                {
+                    if (!character.Equals(prevchar))
+                    {
+                        if (!prevchar.Equals(char.MinValue))
+                        {
+                            if (count > 1)
+                                lst.Add(count.ToString());
+                            lst.Add(prevchar.ToString());
+                        }
+                        count = 1;
+                        prevchar = character;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+                if (count > 1)
+                    lst.Add(count.ToString());
+                if (inpLine.Length > 0)
+                    lst.Add(prevchar.ToString());
+                return string.Join("", lst.ToArray());
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Exception in RLE encoding:" + exc.Message);
+                return null;
+            }
+            
+        }
+
+        public string Decode(string inpLine)
+        {
+            try
+            {
+                int count = 0;
+                List<string> lst = new List<string>();
+
+                foreach (char character in inpLine)
+                {
+                    if (char.IsDigit(character))
+                        count = count * 10 + (int)char.GetNumericValue(character);
+                    else
+                    {
+                        if (count>0)
+                            for (int i = 0; i < count; i++)
+                            {
+                                lst.Add(character.ToString());
+                            }
+                        else
+                            lst.Add(character.ToString());
+                        count = 0;
+                    }
+                }
+
+                return string.Join("", lst.ToArray());
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Exception in RLE decoding:" + exc.Message);
+                return null;
+            }
+            
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
---
+++ RunLengthEncoding/RunLengthEncodingTests/RunLengthEncodingTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RunLengthEncoding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunLengthEncoding.Tests
+{
+    [TestClass()]
+    public class RunLengthEncodingTests
+    {
+        [TestMethod()]
+        public void EncodeTestEmptyString()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("", target.Encode(""));
+        }
+
+        [TestMethod()]
+        public void EncodeTestNoRepeats()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("ABC", target.Encode("ABC"));
+        }
+
+        [TestMethod()]
+        public void EncodeTestNormalInput()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("3A2B4C", target.Encode("AAABBCCCC"));
+        }
+
+        [TestMethod()]
+        public void EncodeTestStringAndSpaces()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("2 3AB", target.Encode("  AAAB"));
+        }
+
+        [TestMethod()]
+        public void EncodeTestLowercase()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("2a3b4c", target.Encode("aabbbcccc"));
+        }
+
+        [TestMethod()]
+        public void DecodeTestEmptyString()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("", target.Decode(""));
+        }
+
+        [TestMethod()]
+        public void DecodeTestSingleChars()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("XYZ", target.Decode("XYZ"));
+        }
+
+        [TestMethod()]
+        public void DecodeTestNoSingleChars()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("AABBBCCCC", target.Decode("2A3B4C"));
+        }
+
+        [TestMethod()]
+        public void DecodeTestCharsWithRepeatedChars()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB", target.Decode("12WB12W3B24WB"));
+        }
+
+        [TestMethod()]
+        public void DecodeTestWhitespaces()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("  hsqq qww  ", target.Decode("2 hs2q q2w2 "));
+        }
+
+        [TestMethod()]
+        public void EncodeAndDecodeTest()
+        {
+            RunLengthEncoding target = new RunLengthEncoding();
+            Assert.AreEqual("zzz ZZ  zZ", target.Decode(target.Encode("zzz ZZ  zZ")));
+        }
+    }
+}

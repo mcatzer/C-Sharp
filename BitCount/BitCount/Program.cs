096458d48ba722b79aee5017c479a5438f24f740
Mcatzer <mcatze@mail.ru>
2018-03-07 18:41:37 +0300

    Counting bits and parity control

---
+++ BitCount/BitCount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCount
+{
+    public static class BitCounter
+    {
+
+        public static int countBits (int what)
+        {
+            int bitnum = 0;
+            uint uwhat = unchecked((uint)what);
+            while (uwhat != 0)
+            {
+                bitnum += (int)(uwhat & 1);
+                uwhat = uwhat >> 1;
+            }
+            return bitnum;
+        }
+
+        // parity control
+        public static string ParityControl(string input)
+        {
+            string[] newInp = input.Split(' ');
+            StringBuilder outp = new StringBuilder();
+            foreach (string num in newInp)
+            {
+                outp.Append( countBits(int.Parse(num)) % 2 == 0 ? Convert.ToString( (char)(int.Parse(num) & 127) ) : "");
+            }
+            return (outp.ToString());
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitCounter.ParityControl(Console.ReadLine());
+        }
+    }
+}
---
+++ BitCount/BitCountTests/BitCounterTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitCount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCount.Tests
+{
+    [TestClass()]
+    public class BitCounterTests
+    {
+        [TestMethod()]
+        public void countBitsTest1()
+        {
+            Assert.AreEqual(1, BitCounter.countBits(1));
+        }
+
+        [TestMethod()]
+        public void countBitsTest100()
+        {
+            Assert.AreEqual(3, BitCounter.countBits(100));
+        }
+
+        [TestMethod()]
+        public void countBitsTestMinus1()
+        {
+            Assert.AreEqual(32, BitCounter.countBits(-1));
+        }
+
+    }
+}
